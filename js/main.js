ffd1a906f4d1804b5156891f08cf1e210c229b49
ragnar-pwninskjold <seankochel@gmail.com>
2016-07-24 15:00:59 -0400

    trying to restore stash

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	//alert("This tool will enable you to search Amazon.com for a string of text in a specific category, and will return opportunities for price arbitrage given Alibaba wholesaler prices for a given product. For example: If you chose 'Home and furniture' category and entered a search string with a range for prices and average sales rank of the product you wanted, it would return a table of opportunities to buy from Alibaba wholesaler and sell through Amazon. It will calculate the total expected profit margin as well");
 
- $( "#slider-range" ).slider({
+	$( "#slider-range" ).slider({
       range: true,
       min: 0,
       max: 100,
@@ -15,7 +15,7 @@ $(document).ready(function() {
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
 
 
-$( "#slider-range2" ).slider({
+	$( "#slider-range2" ).slider({
       range: true,
       min: 0,
       max: 100,
@@ -27,16 +27,16 @@ $( "#slider-range2" ).slider({
     $( "#amount2" ).val($( "#slider-range2" ).slider( "values", 0 ) +
       " - " + $( "#slider-range2" ).slider( "values", 1 ) );
 
-//submit for the text string
-$('.inputbox').submit( function(event){
+	//submit for the text string
+	$('.inputbox').submit( function(event){
 		event.preventDefault();
 		// zero out results if previous search has run
 		// get the value of the tags the user submitted
 		var tag = $(this).find("input[id='sell']").val();
 		console.log(tag);
-		amazonRequest(tag);
-		//ebayRequest();
-
+		var amzn = amazonRequest(tag);	
+		console.log(amzn);
+		var bay = ebayRequest(amzn);
 	});
 });
 
@@ -50,38 +50,30 @@ function amazonRequest(tags) {
 	$.ajax({
 		url: "http://localhost:8888/amazonphpapi/index.php",
 		data: params,
-		type: "GET",
+		type: "GET"
 	})
 	.done(function(result) {
 		it = JSON.parse(result);
 		items = it.Items.Item;
 		console.log(items);
 		$(".results-table").show();
-			for (i=0;i<9;i++) {
-			var newRow = $(".table-data").clone();
-			//attach url to title eventually
+		for (i=0;i<1;i++) {
 			var url = items[i].DetailPageURL;
-			var title = items[i].ItemAttributes.Title;
-			var titleSpot = newRow.find('.title');
-    		titleSpot.text(title);
+			title = items[i].ItemAttributes.Title;
+    		console.log(title);
+    		//console.log(eRequest);
 			var salesRank = items[i].SalesRank;
-			var salesRankSpot = newRow.find('.rank');
-			salesRankSpot.text(salesRank);
 			var avgReview = items[i].Review;
-			var avgReviewSpot = newRow.find('.rating');
-			avgReviewSpot.text(avgReview);
 			var price = items[i].OfferSummary.LowestNewPrice.FormattedPrice;
-			var priceSpot = newRow.find('.price');
-			priceSpot.text(price);
-			var categorySpot = newRow.find(".category");
-			categorySpot.text("placeholder-category");
-			var ebaySpot = newRow.find(".ebay-name");
-			ebaySpot.text("placeholder ebay name");
-			var ebayPriceSpot = newRow.find(".ebay-price");
-			ebayPriceSpot.text("placeholder ebay price");
-			$(".results-table").append(newRow);
+			var category = "placeholder amazon category";
+			var ebayTitle = "placeholder ebay title";
+			var ebayShipping = "placeholder shipping cost";
+			var ebayPrice = "placeholder ebay price";
+			//ebayRequest(title);
+			rowEntries = [title, url, salesRank, avgReview, price, category, ebayTitle, ebayShipping, ebayPrice];
 		}
 	});
+
 	//.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
 	//	var errorElem = showError(error);
 	//	$('.search-results').append(errorElem);
@@ -89,26 +81,58 @@ function amazonRequest(tags) {
 }
 
 
-/*
-function ebayRequest() {
-	var url = "http://svcs.ebay.com/services/search/FindingService/v1";
-    url += "?OPERATION-NAME=findItemsByKeywords";
-    url += "&SERVICE-VERSION=1.0.0";
-    url += "&SECURITY-APPNAME=SeanKoch-APICapst-SBX-d09b8ef6f-e9696e94";
-    url += "&GLOBAL-ID=EBAY-US";
-    url += "&RESPONSE-DATA-FORMAT=JSON";
-    url += "&callback=_cb_findItemsByKeywords";
-    url += "&REST-PAYLOAD";
-    url += "&keywords=harry%20potter";
-    url += "&paginationInput.entriesPerPage=3";
-	console.log(url); 
-	}
 
-	var params {}
+function ebayRequest(ebayTag) {
+	var params = {
+		"OPERATION-NAME": "findItemsByKeywords",
+		"SERVICE-VERSION": "1.0.0",
+		"SECURITY-APPNAME": "SeanKoch-APICapst-PRD-313d8b941-9995d360",
+		"GLOBAL-ID": "EBAY-US",
+		"RESPONSE-DATA-FORMAT": "JSON",
+		"callback": "_cb_findItemsByKeywords",
+		//Why won't ebayTag work for this specific search phrase?
+		//it won't work for 'coffee', but will work for 'blender'
+		//something to do with the string?
+		//in testing this, certain strings return undefined, particularly those using -
+		"keywords": ebayTag,
+		"paginationInput.entriesPerPage": "5"
+	};
+	
+	$.ajax({
+		url: "http://svcs.ebay.com/services/search/FindingService/v1",
+		data: params,
+		dataType: "jsonp",
+		type: "GET"
+	})
+	.done(function(result) {
+		console.log(result);
+		ebayPrice = result.findItemsByKeywordsResponse[0].searchResult[0].item[0].sellingStatus[0].currentPrice[0].__value__;
+		ebayTitle = result.findItemsByKeywordsResponse[0].searchResult[0].item[0].title[0];
+		ebayShipping = result.findItemsByKeywordsResponse[0].searchResult[0].item[0].shippingInfo[0].shippingServiceCost[0].__value__;
+		ebayArray = [ebayTitle, ebayShipping, ebayPrice];
+		console.log(ebayArray);
+	});
+//	return ebayArray;
+}
 
-
-
-*/
-
-
-
+function makeRow(rowArray) {
+	var newRow = $(".table-data").clone();
+	$(".results-table").show();
+		var titleSpot = newRow.find('.title');
+    	titleSpot.text(rowArray[0]);
+		var salesRankSpot = newRow.find('.rank');
+		salesRankSpot.text(rowArray[2]);
+		var avgReviewSpot = newRow.find('.rating');
+		avgReviewSpot.text(rowArray[3]);
+		var priceSpot = newRow.find('.price');
+		priceSpot.text(rowArray[4]);
+		var categorySpot = newRow.find(".category");
+		categorySpot.text(rowArray[5]);
+		var ebayName = newRow.find(".ebay-name");
+		ebayName.text(rowArray[6]);
+		var ebayPriceSpot = newRow.find(".ebay-price");
+		ebayPriceSpot.text(rowArray[8]);
+		ebayShipping = newRow.find(".ebay-shipping");
+		ebayShipping.text(rowArray[7]);
+		$(".results-table").append(newRow);
+}
